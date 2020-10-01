e28afb52b06df645e5a15eaeb310063ba8316eb9
Emmanuel <kolemsy@gmail.com>
2020-10-01 11:55:11 +0100

    Yelp API integrated

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+const apiKey = 'IcLvj6Bbf3P-8E-5XY_Naxlvipty81iv52HWlGt60f8_d_vtj9JTvhP_RJrtMoA4xaxBmInNPyunUfS8MLYoZLABv-oPMpZyPVJEtqAY9ZcFjy-jhprpsW6SrE1sX3Yx';
+
+const Yelp = {
+    search(term, location, sortBy){
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+             headers: {Authorization: `Bearer ${apiKey}`
+        }
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse =>{
+        if(jsonResponse.businesses){
+            return jsonResponse.businesses.map(business =>{
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city:business.location.city,
+                    state:business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                };
+            });
+        }
+    });
+    }
+};
+
+
+export default Yelp;
